495255d9bdf41afbf79a90fbb32bff2012533c0a
oriedofelo@gmail.com <oriedofelo@gmail.com>
2019-06-13 12:50:01 +0300

    shortcode updates

--- src/main/java/ke/co/creswave/uzimahealth/UzimaHealthApplication.java
+++ src/main/java/ke/co/creswave/uzimahealth/UzimaHealthApplication.java
@@ -1,12 +1,16 @@
 package ke.co.creswave.uzimahealth;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+@EnableAutoConfiguration
+@SpringBootApplication
 public class UzimaHealthApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(UzimaHealthApplication.class, args);
---
+++ src/main/java/ke/co/creswave/uzimahealth/controllers/ShortCodeController.java
@@ -0,0 +1,29 @@
+package ke.co.creswave.uzimahealth.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ke.co.creswave.uzimahealth.model.ApiResponse;
+import ke.co.creswave.uzimahealth.model.Doctor;
+
+@Controller
+@RequestMapping(value = "/shortcode")
+public class ShortCodeController {
+	@GetMapping("/hello-world")
+	@ResponseBody
+	public ApiResponse sayHello(@RequestParam(name = "from") String from) {
+		return new ApiResponse("success", 200);
+	}
+}
